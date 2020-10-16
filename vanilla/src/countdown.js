c1f44a726c48dd481080c926e29fadafef98c3d1
Stéphane Vanberg <vanbergstephane@gmail.com>
2020-10-16 21:11:13 +0200

    Cleanup stylesheet

--- vanilla/src/countdown.js
+++ vanilla/src/countdown.js
@@ -1,7 +1,9 @@
 import { draw } from './draw';
 import { sleep } from './utils';
 
-export default async (svg) => {
+export default async (svg, options = {
+
+}) => {
     for (let i = 10; i >= 0; i--) {
         draw(svg, i);
         await sleep(1000);
